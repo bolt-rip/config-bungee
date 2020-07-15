aef184cf5a2b5c05d1026509ab7766621443bcdb
Emilien Devos <contact@emiliendevos.be>
2020-07-15 20:42:10 +0200

    include secrets in template using yq

---
+++ bin/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+
+yq m -i /minecraft/plugins/Nerve/template.yml /minecraft/plugins/Nerve/template-secrets.yml
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+/usr/bin/startup.sh
+
 exec java -jar bungeecord.jar
